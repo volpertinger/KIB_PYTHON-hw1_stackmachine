68da2723e528bf98b32a425a0323c457231f5252
volpertinger <merzlovnik@mail.ru>
2021-03-28 00:49:10 +0300

    in SM compilator: solved address problem

--- StackMachineCompilator.py
+++ StackMachineCompilator.py
@@ -4,6 +4,9 @@ import tokenize
 
 
 def compile_file(filename):
+    instructions = ['*', '%', '+', '-', '/', '==', 'println', 'print', 'cast_int', 'cast_str', 'drop', 'dup', 'if',
+                    'jmp', 'stack', 'swap', 'read', 'read_int', 'call', 'return', 'exit', 'store',
+                    'load']  # stack for commands
     raw_code = []
     with tokenize.open(filename) as f:
         tokens = tokenize.generate_tokens(f.readline)
@@ -15,7 +18,7 @@ def compile_file(filename):
             if f_comment and tokval == "\n":
                 f_comment = False
                 continue
-            if tokval == "\n" or tokval == "', '":
+            if tokval == "\n" or tokval == "', '" or tokval == "":
                 continue
             if toknum == tokenize.NUMBER and not f_comment:
                 raw_code.append(int(tokval))
@@ -23,12 +26,12 @@ def compile_file(filename):
             elif not f_comment:
                 raw_code.append(tokval)
         f.close()
-    procedure_depth = 0  # 0-main, 1,2,3... - for other procedures
+        raw_code.append("exit")  # adding exit - command at the end of code
 
     main_code = []
     procedure_code = []
 
-    procedure_depth = 0;
+    procedure_depth = 0
     for element in raw_code:
         if element == ":":
             procedure_depth += 1
@@ -39,12 +42,18 @@ def compile_file(filename):
             procedure_code.append(element)
             continue
         if procedure_depth:
+            if instructions.count(element) == 0 and element.find('"'):
+                procedure_code.append("%address%")
             procedure_code.append(element)
         else:
+            if instructions.count(element) == 0 and element.find('"'):
+                main_code.append("%address%")
             main_code.append(element)
+    # --------------------------
     print(raw_code)
     print(main_code)
     print(procedure_code)
+    # -------------------------
     return raw_code
 
 
